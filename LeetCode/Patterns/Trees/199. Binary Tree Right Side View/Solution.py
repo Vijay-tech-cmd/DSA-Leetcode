e8ec799853da715c9643aa131c7990230a9aa204
Jada Vijay <jadavijay555@gmail.com>
2026-08-11 17:58:44 +0530

    Add Python Solution to Patterns/Trees

---
+++ LeetCode/Patterns/Trees/199. Binary Tree Right Side View/Solution.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        level = 0
+        ans = []
+        def Right(node, level):
+            if not node:
+                return
+            if level == len(ans):
+                ans.append(node.val)
+            Right(node.right, level + 1)
+            Right(node.left, level + 1)
+        Right(root, level)
+        return ans
