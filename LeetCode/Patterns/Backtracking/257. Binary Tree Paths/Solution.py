656744a4891dbe59bed81d0588a2401c62071fe0
Jada Vijay <jadavijay555@gmail.com>
2026-08-12 18:00:33 +0530

    Add Python Solution to Patterns/Backtracking

---
+++ LeetCode/Patterns/Backtracking/257. Binary Tree Paths/Solution.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
+        ans = []
+        def dfs(node, path):
+            if not node:
+                return 
+            path.append(node.val)
+            if node.left is None and node.right is None:
+                ans.append("->".join(map(str, path[:])))
+            dfs(node.left, path)
+            dfs(node.right, path)
+
+            path.pop()
+        dfs(root, [])
+        return ans
