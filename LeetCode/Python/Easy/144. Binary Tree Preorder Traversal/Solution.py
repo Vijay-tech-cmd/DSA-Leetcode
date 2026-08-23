776f39f61a3f775c8e0ab3dacbe18adc6187226a
Jada Vijay <jadavijay555@gmail.com>
2026-08-23 16:27:03 +0530

    Add Easy Python Solution from LeetCode

--- LeetCode/Python/Easy/144. Binary Tree Preorder Traversal/Solution.py
+++ LeetCode/Python/Easy/144. Binary Tree Preorder Traversal/Solution.py
@@ -6,12 +6,21 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        ans = []
-        def preorder(node):
-            if not node :
-                return
-            ans.append(node.val)
-            preorder(node.left)
-            preorder(node.right)
-        preorder(root)
-        return ans
+        preorder = []
+        curr = root
+        while curr:
+            if curr.left is None:
+                preorder.append(curr.val)
+                curr = curr.right
+            else:
+                prev = curr.left
+                while prev.right is not None and prev.right != curr:
+                    prev = prev.right
+                if prev.right is None:
+                    prev.right = curr
+                    preorder.append(curr.val)
+                    curr = curr.left
+                else:
+                    prev.left = None
+                    curr = curr.right
+        return preorder
