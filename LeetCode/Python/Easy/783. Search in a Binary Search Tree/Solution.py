bed92c6a20ce0ee0f37cb809330c9966e99d738b
Jada Vijay <jadavijay555@gmail.com>
2026-08-30 21:00:25 +0530

    Add Easy Python Solution for 783. Search in a Binary Search Tree from LeetCode

--- LeetCode/Python/Easy/783. Search in a Binary Search Tree/Solution.py
+++ LeetCode/Python/Easy/783. Search in a Binary Search Tree/Solution.py
@@ -1,14 +1,16 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def searchBST(self, root, val):
-        if not root or root.val == val:
-            return root
-        if val < root.val:
-            return self.searchBST(root.left,val)
-        else:
-            return self.searchBST(root.right,val)
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        if not root:
+            return 
+        while root != None and root.val != val:
+            if val > root.val:
+                root = root.right
+            else:
+                root = root.left
+        return root
