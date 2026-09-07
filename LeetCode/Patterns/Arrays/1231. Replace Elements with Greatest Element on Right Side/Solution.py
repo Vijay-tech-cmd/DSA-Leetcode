413512469a51edc4d6dbfd210d7165917b9a14c7
Jada Vijay <jadavijay555@gmail.com>
2026-09-07 19:59:09 +0530

    Add Easy Python Solution for 1231. Replace Elements with Greatest Element on Right Side from LeetCode

---
+++ LeetCode/Patterns/Arrays/1231. Replace Elements with Greatest Element on Right Side/Solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        max_value = arr[-1]
+        arr[-1] = -1
+        for i in range(len(arr)-2,-1,-1):
+            temp = arr[i]
+            arr[i] = max_value
+            if temp > max_value : max_value = temp
+        return arr
